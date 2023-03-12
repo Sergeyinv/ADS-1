22e2930be05635e2c9aff9962463267ffb97bdc5
Sergeyinv <125216664+Sergeyinv@users.noreply.github.com>
2023-03-13 02:17:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,58 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+uint64_t count = 0;
+  for (int i = 2; i*i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t count1 = 0, count2 = 0;
+  for (int i = 1; i <= 10000; i++) {
+    for (int j = i; j >= 1; j--) {
+      if (i % j == 0) {
+        count1++;
+      }
+    }
+    if (count1 == 2) {
+      count2++;
+      count1 = 0;
+    }
+    if (count2 == n) {
+      return i;
+    }
+      count1 = 0;
+  }
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t count = 0;
+  for (int i = value + 1; i <= 10000; i++) {
+    for (int j = i; j >= 1; j--) {
+      if (i % j == 0) {
+        count++;
+      }
+    }
+    if (count == 2) {
+      return i;
+      count = 0;
+    }
+      count = 0;
+  }
+  return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t count = 0, sum = 0;
+  for (int i = 0; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum - 1;
 }
